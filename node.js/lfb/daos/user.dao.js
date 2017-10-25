9b854ba36be9e315250d682e19b991ddc3cc6c6c
baoxianjian <baoxianjian@gmail.com>
2017-10-25 11:24:54 +0800

    users' posts list

--- node.js/lfb/daos/user.dao.js
+++ node.js/lfb/daos/user.dao.js
@@ -1,7 +1,10 @@
 'use strict';
 const dbo = require("./dbo");
-
+//const uuidv5 = require('uuid/v5');
 var user = require("../models/user.js");
+var {ObjectId} = require('mongodb'); 
+
+
 
 
 var userDAO = function () {
@@ -14,8 +17,40 @@ var userDAO = function () {
 
 	this.getUserByEmailAndPwd = function (email,pwd,callback) {
 		dbo.execute(db=>{return db.collection(tableName).findOne({email:email,password:pwd});},callback);
-        //return userList.filter(a=>{return a.id==id}).pop();
     }
+	
+	this.getAllPosts = function(type,uid, callback){
+		var condition={"posts.type":type};
+	
+		if(uid)
+		{
+			if(!dbo.isValidObjectId(uid))
+			{
+				callback({});
+				return;
+			} 
+			condition._id = dbo.safeObjectId(uid);
+		}
+		
+		console.log(condition);
+
+		dbo.execute(db=>{return db.collection(tableName)
+		.aggregate(
+				[{"$unwind":"$posts"},{"$match":{"posts.type":parseInt(type)}},{"$project":{"posts":1,"_id":0}}],
+				(err, result)=>{callback(result);}
+			);
+		},null);
+		
+		
+		
+		
+		//dbo.execute(db=>{return db.collection(tableName).findOne({"posts.type":0});},callback);
+		
+		
+	}
+	
+	
+	
 	
 	
 }
