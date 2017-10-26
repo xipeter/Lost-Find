9f969ac64d1038f7b0caa3475566dd7c07d7fe05
xipeter <xizongfei@gmail.com>
2017-10-25 20:12:49 -0500

    Revert "Fix user object in signup component"
    This reverts commit 3db68c241b1a8f35664789c9133529b66aa0ce19.

--- angular/App/src/app/signup.component.ts
+++ angular/App/src/app/signup.component.ts
@@ -20,7 +20,10 @@ export class SignupComponent {
 
     imagePath = '/assets/img/signup.jpg';
 
+    ret:ReturnObj;
+
     constructor(
+        private user: User,
         private userService: UserService,
         private http: HttpClient,
         private router: Router,
@@ -42,30 +45,38 @@ export class SignupComponent {
 
     register(form) {
 
-        let user = {
-            fn: form.value.firstName,
-            ln: form.value.lastName,
-            email: form.value.email,
-            pwd: form.value.password
-        }
+        this.user.fn = form.firstName;
+        this.user.ln = form.lastName;
+        this.user.email = form.email;
+        this.user.pwd = form.password;
 
+        console.log(this.ret);
+
+<<<<<<< HEAD
 <<<<<<< HEAD
         this.http.post('http://155.254.33.141:3000/api/users', JSON.stringify(this.user))
 =======
         this.http.post('http://155.254.33.141:3000/api/users', user)
 >>>>>>> a274fdc5490aaec6fa2a73f22ca5a69c71a165e4
+=======
+        this.http.post('http://155.254.33.141:3000/api/users', this.user)
+>>>>>>> parent of 3db68c2... Fix user object in signup component
             .subscribe(
                 data => {
                     if(data['status'] == 1){
                         this.returnObj.code = 1;
                         this.returnObj.Message = 'Sign up succeed!';
 <<<<<<< HEAD
+<<<<<<< HEAD
                         console.log(data);
                         // this.router.navigate(['/home']);
 =======
                         // this.router.navigate(['/home']);
                         console.log(data);
 >>>>>>> a274fdc5490aaec6fa2a73f22ca5a69c71a165e4
+=======
+                        this.router.navigate(['/home']);
+>>>>>>> parent of 3db68c2... Fix user object in signup component
 
                     }else{
                         this.returnObj.code = 0;
